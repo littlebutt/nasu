f209ee4cbf57345cf3673b0eaa71096d7eb6be19
littlebutt <luogan199686@gmail.com>
2023-01-11 20:20:25 +0800

    chore: react app init

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import "./components/button"
+
+function App() {
+  return (
+    <div>
+        <button className="ui button">Click Here</button>
+    </div>
+  );
+}
+
+export default App;
